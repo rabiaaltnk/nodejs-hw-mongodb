32629096f317c3f9f4f15644281d916872bd225b
rabia altinok <rabiaaltinok99@icloud.com>
2025-10-10 21:29:45 +0300

    second commit

--- src/server.js
+++ src/server.js
@@ -1,22 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const pinoHttp = require('pino-http');
 
-const { getAllContactsController, getContactByIdController } = require('./controllers/contacts');
+const express = require("express");
+const cors = require("cors");
+const pinoHttp = require("pino-http");
+
+const {
+  getAllContactsController,
+  getContactByIdController,
+} = require("./controllers/contacts");
 
 function setupServer() {
   const app = express();
 
+
   app.use(cors());
-  app.use(pinoHttp());
+  app.use(express.json());
+  app.use(
+    pinoHttp({
+      transport: {
+        target: "pino-pretty",
+      },
+    })
+  );
 
-  
-  app.get('/contacts', getAllContactsController);
-  app.get('/contacts/:contactId', getContactByIdController);
+  app.get("/api/contacts", getAllContactsController);
+  app.get("/api/contacts/:contactId", getContactByIdController);
 
-  
   app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
+    res.status(404).json({ message: "Not found" });
+  });
+
+
+  app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).json({ message: "Server error" });
   });
 
   return app;
